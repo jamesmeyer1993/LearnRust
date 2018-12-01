94adfa5789e6d5b876a6855b60d209b4249c7654
jamesmeyer1993 <jmeyer1993@outlook.com>
2018-12-01 11:58:52 -0600

    Breakthrough with unsafe blockgit add .!

--- curltest/src/main.rs
+++ curltest/src/main.rs
@@ -1,21 +1,11 @@
 extern crate curl;
 
-use std::io::{stdout, Write};
 use std::vec::Vec;
 use curl::easy::Easy;
 
-// Write the contents of a webpage to stdout
-fn http_get_print(url: &String) {
-    let mut easy = Easy::new();
-    easy.url(url).unwrap();
-    easy.write_function(|data| {
-        Ok(stdout().write(data).unwrap())
-    }).unwrap();
-    easy.perform().unwrap();
-}
-
-fn http_get_to_vec(url: &String) {
-    let mut dst = Vec::new();
+fn http_get(url: &String) -> Vec<u8> {
+    let mut dst: Vec<u8> = Vec::new();
+    //let mut buff = String::new();
     let mut easy = Easy::new();
     easy.url(url).unwrap();
 
@@ -23,33 +13,24 @@ fn http_get_to_vec(url: &String) {
         let mut transfer = easy.transfer();
         transfer.write_function(|data| {
             dst.extend_from_slice(data);
+            //buff = String::from_utf8_lossy(data).to_string();
             Ok(data.len())
         }).unwrap();
         transfer.perform().unwrap();
     }
-
-    println!("Vec len = {}", dst.len());
-}
-
-// this function does not compile. Need to learn more about vectors to get it straight.
-fn http_get_to_vec2(url: &String) {
-    let mut dst: Vec<u8> = Vec::new();
-    let mut easy = Easy::new();
-    easy.url(url).unwrap();
-    {
-        let mut transfer = easy.transfer();
-        transfer.write_function(|data| {
-            let len = data.len();
-            dst.append(&mut data); // apparently faster
-            Ok(len)
-        }).unwrap();
-    }
-    println!("Vec len = {}", dst.len());
+    return dst;
+    //println!("Vec len = {}", dst.len());
 }
 
 fn main() {
 
-    //http_get_print(&String::from("https://www.reddit.com"));
-    //http_get_to_vec(&String::from("https://www.reddit.com"));
-    http_get_to_vec2(&String::from("https://www.reddit.com"));
+    let domain = String::from("https://www.google.com");
+    let mut u8_page: Vec<u8> = http_get(&domain);
+    println!("{} bytes collected from {}\n", u8_page.len(), domain);
+    //let mut str_page = String::from_utf8(u8_page).expect("Found invalid UTF-8");
+
+    unsafe {
+        let str_page = String::from_utf8_unchecked(u8_page);
+        println!("{}", str_page);
+    }
 }
