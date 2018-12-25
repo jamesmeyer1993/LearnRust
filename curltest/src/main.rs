5fe8689e48d656350d7d0c7571e765851489c6e8
jamesmeyer1993 <jmeyer1993@outlook.com>
2018-12-25 15:45:53 -0600

    updated

--- curltest/src/main.rs
+++ curltest/src/main.rs
@@ -3,34 +3,66 @@ extern crate curl;
 use std::vec::Vec;
 use curl::easy::Easy;
 
-fn http_get(url: &String) -> Vec<u8> {
+struct HtmlElem {
+    start: Option<String>,
+    end: Option<String>,
+}
+
+impl HtmlElem {
+    fn new_div() -> HtmlElem {
+        HtmlElem {
+            start: Some(String::from("<div>")),
+            end: Some(String::from("</div>")),
+        }
+    }
+}
+
+fn http_get(url: &String) -> String {
     let mut dst: Vec<u8> = Vec::new();
-    //let mut buff = String::new();
+
     let mut easy = Easy::new();
     easy.url(url).unwrap();
-
     {
         let mut transfer = easy.transfer();
         transfer.write_function(|data| {
             dst.extend_from_slice(data);
-            //buff = String::from_utf8_lossy(data).to_string();
             Ok(data.len())
         }).unwrap();
         transfer.perform().unwrap();
     }
-    return dst;
-    //println!("Vec len = {}", dst.len());
+
+    let mut buff;
+    unsafe {
+        buff = String::from_utf8_unchecked(dst);
+    }
+    return buff;
 }
 
 fn main() {
 
     let domain = String::from("https://www.google.com");
-    let mut u8_page: Vec<u8> = http_get(&domain);
-    println!("{} bytes collected from {}\n", u8_page.len(), domain);
-    //let mut str_page = String::from_utf8(u8_page).expect("Found invalid UTF-8");
+    let page: String = http_get(&domain);
+    println!("{}", page);
 
-    unsafe {
-        let str_page = String::from_utf8_unchecked(u8_page);
-        println!("{}", str_page);
+    let mut start: i32 = 0;
+    let mut end: i32 = 0;
+    match page.find("<style>") {
+        Some(x) => start = x as i32,
+        None => start = -1,
+    }
+    match page.find("</style>") {
+        Some(x) => end = x as i32,
+        None => end = -1,
+    }
+
+    let div = HtmlElem::new_div();
+    let elements: Vec<_>;
+    match div.start {
+        Some(s) => elements = page.match_indices(s).collect(),
+        None => panic!(""),
+    }
+
+    for int in elements.iter() {
+        println!("{}", int);
     }
 }
